fef2fcbc327e03460232914e96409c81d9c07f63
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-07-08 16:51:08 -0300

    add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import firebase from './service/fireabase';
+
+import firebase from './service/firebase';
 
 import Loading from './components/Loading';
 import Header from './components/Header';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/DashBoard';
+import New from './pages/New';
 
 
 export default class Routes extends Component {
@@ -16,9 +20,14 @@ export default class Routes extends Component {
 	}
 
 	componentDidMount() {
+
+		firebase.handleLogin('gustav@gmail.com','123456');
+
 		firebase.isLogged().then(response => {
 			this.setState({ firebaseInitialized: response })
-		})
+
+		});
+
 	}
 
 	render() {
@@ -31,10 +40,13 @@ export default class Routes extends Component {
 					<Switch>
 						<Route exact path='/' component={Home} />
 						<Route exact path='/login' component={Login} />
+						<Route exact path='/register' component={Register} />
+						<Route exact path='/dashboard' component={Dashboard} />
+						<Route exact path='/dashboard/new' component={New} />
 					</Switch>
 				</BrowserRouter>
-			):
-			<Loading />
+			) :
+				<Loading />
 		)
 	}
 }
